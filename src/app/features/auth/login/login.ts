fa221de6f3facedfc6b8ab563a73d90c7d5eb889
Vysakh P <vysakhp17@gmail.com>
2026-05-10 00:35:14 +0530

    feat: enhance login functionality with form validation and remove unused spec file

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { LoginRequest } from '../../../core/models/login';
+import { form, FormField, required, submit } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormField],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+
+  protected loginModel = signal<LoginRequest>({
+    email: '',
+    password: ''
+  });
+
+  protected loginForm = form(this.loginModel, (schemaPath) => {
+    required(schemaPath.email, { message: 'Email is required' });
+    required(schemaPath.password, { message: 'Password is required' });
+  });
+
+
+  protected async onSubmit() {
+    // 1. At this point all fields are already marked as touched
+    // 2. If form is invalid - this function will NOT be called
+    // 3. f().submitting() === true during execution
+    await submit(this.loginForm, async (f) => {
+      if (!f().valid) {
+        console.log('Form is invalid:', f().errors);
+        return;
+      }
+      // Handle form submission, e.g., call an authentication service
+      console.log('Form submitted with:', f().value());
+    });
+  }
+}
